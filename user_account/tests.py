e0d6a17980a462413940ba291333ffec903e42e0
Vincent74230 <vincent.nowak@hotmail.fr>
2020-10-05 10:50:36 +0200

    ADDED : medallion shows nutriscore

--- user_account/tests.py
+++ user_account/tests.py
@@ -1,14 +1,32 @@
 from django.test import TestCase
 from django.urls import reverse
-import unittest
-from unittest import mock
 
-class TestIndex(TestCase):
+
+
+class IndexPageTest(TestCase):
     def test_index_page_user_not_authenticated(self):
         response=self.client.get(reverse('user_account'))
         self.assertEqual(response.status_code, 302)
 
-class TestLogout(TestCase):
+class LogoutTest(TestCase):
     def test_user_logout_redirect(self):
         response=self.client.get(reverse('logout'))
         self.assertEqual(response.status_code, 302)
+'''
+    def setUp(self):
+        user=
+'''
+class SignInTest(TestCase):
+    def test_sign_in(self):
+        response=self.client.get('/user_account/sign_in')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sign_in_post(self):
+        response= self.client.post('/user_account/sign_in', {
+            'username':'Vince74',
+            'last_name':'NOWACZYK',
+            'first_name':'Vincent',
+            'email':'vince@gmail.com',
+            'password':'Radeon74'
+            })
+        self.assertEqual(response.status_code, 200)
